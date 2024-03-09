79aa08e13b97b6a195bdcb1042bf8e5e316fd9cf
PavloPosternak <posternakpavlo98@gmail.com>
2024-03-09 17:34:55 +1000

    homepage complete

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,45 +4,51 @@ import Link from "next/link"
 import sms from "../../public/sms-notification.svg"
 import secure from "../../public/shield-security.svg"
 import eye from "../../public/eye-slash.svg"
+import { Roboto } from "next/font/google"
+
+const roboto = Roboto({
+    weight: '400',
+    subsets: ['latin'],
+  })
 
 export default (props : any) =>{
 
     // const [email, setEmail] = useState("")
 
     return(
-        <div className="flex justify-center pt-[140px]">
-            <div className=" w-[384px] h-[495px] login px-[24px]">
-                <div className=" w-full py-[32px] text-center font-bold text-[20px] leading-[20px]  ">Sign In</div>
-                <div className=" gap-[12px] flex flex-col justify-center">
-                    <p className="w-full py-0 font-bold text-[13.3px] leading-[16.63px]">Email</p>
-                    <div className="w-full h-[52px] rounded-[8px] px-[20px] py-[14px] border-[2.22px] border-[#F2F3F5] flex ">
-                        <Image src={sms} alt="asvg" />
-                        <input type="email" className=" focus:outline-none px-[15px] py-[4px]" placeholder="Enter your email" />
+        <div className={`${roboto.className} px-[10rem]`}>
+            <div className="sm:mx-[50rem] login px-[10rem] py-[10rem] rounded-[2rem]">
+                <div className="sm:text-[6rem] sm:leading-[10rem] sm:mb-[3rem] text-[16rem] leading-[20rem] text-center mb-[10rem]">Sign In</div>
+                <div className="sm:mb-[3rem] mb-[8rem]">
+                    <p className="sm:text-[3rem] sm:mb-[2rem] font-semibold text-[9rem] mb-[4rem]">Email</p>
+                    <div className="sm:px-[3rem] sm:py-[2rem] sm:rounded-[1rem] sm:gap-[3rem] items-center flex px-[7rem] py-[4rem] gap-[7rem] border-[1px] border-[#F2F3F5] rounded-[4rem]">
+                        <Image src={sms} alt="asvg" className=" sm:h-[4rem] h-[13rem] w-auto" />
+                        <input type="email" className=" sm:text-[3rem] sm:leading-[4rem] text-[8.5rem] leading-[9rem] focus:outline-none" placeholder="Enter your email" />
                     </div>
                 </div>
-                <div className=" gap-[12px] flex flex-col justify-center mt-[20px]">
-                    <p className="w-full py-0 font-bold text-[13.3px] leading-[16.63px]">Password</p>
-                    <div className="w-full justify-between h-[52px] rounded-[8px] px-[20px] py-[14px] border-[2.22px] border-[#F2F3F5] flex">
-                        <Image src={secure} alt="asecure" />
-                        <input type="password" className="flex flex-1 focus:outline-none px-[15px] py-[4px] " placeholder="Enter your password" />
-                        <Image src={eye} alt="aeye" className=" cursor-pointer" />
+                <div className=" mb-[4rem]">
+                    <p className="sm:text-[3rem] sm:mb-[2rem] font-semibold text-[9rem] mb-[4rem]">Password</p>
+                    <div className="sm:px-[3rem] sm:py-[2rem] sm:rounded-[1rem] sm:gap-[3rem] items-center flex px-[7rem] py-[4rem] gap-[7rem] border-[1px] border-[#F2F3F5] rounded-[4rem]">
+                        <Image src={secure} alt="asecure"  className=" sm:h-[4rem] h-[13rem] w-auto"/>
+                        <input type="password" className=" sm:text-[3rem] sm:leading-[4rem] w-[80%] text-[8.5rem] leading-[9rem] focus:outline-none " placeholder="Enter your password" />
+                        <Image src={eye} alt="aeye" className=" sm:h-[4rem] h-[13rem] w-auto " />
                     </div>
                 </div>
-                <div className=" mt-[20px] flex flex-row ">
-                <label className="checkgroup text-[14px] leading-[20px] text-[#828A91] font-medium">You accept the terms and conditions?
+                <div className="flex items-center mb-[3rem]">
+                <label className="sm:text-[2.5rem] sm:mr-[1.5rem] checkgroup text-[7rem] text-[#828A91] mr-[3rem]">You accept the terms and conditions?
                     <input type="checkbox" />
                     <span className="checkmark"></span>
                 </label>
-                <Link href="#" className=" pl-[5px] underline text-[14px] leading-[20px] text-[#17191C] font-medium">Terms</Link>
+                <Link href="#" className="sm:text-[2.5rem] text-[7rem] font-medium underline">Terms</Link>
                 </div>
-                <div className=" mt-[12px] flex flex-row">
-                    <p className="text-[14px] leading-[20px] text-[#828A91] font-medium">Forgot your password?</p>
-                    <Link href="#" className=" pl-[5px] underline text-[14px] leading-[20px] text-[#17191C] font-medium">Recover Password</Link>
+                <div className="sm:mb-[5rem] flex items-center mb-[16rem]">
+                    <p className="sm:text-[2.5rem] sm:mr-[2rem] text-[7rem] text-[#828A91] mr-[3rem]">Forgot your password?</p>
+                    <Link href="#" className="sm:text-[2.5rem] text-[7rem] font-medium underline">Recover Password</Link>
                 </div>
-                <div className=" mt-[20px] cursor-pointer text-[white] text-[15.52px] leading-[44px] text-center font-bold w-full h-[44px] rounded-[8px] login-button">Sign In</div>
-                <div className=" mt-[25px] flex flex-row justify-center">
-                    <p className="text-[15px] text-[#828A91] leading-[15px] font-medium">Don’t have an account?</p>
-                    <Link href="#" className="text-[15px] text-[#020318] leading-[15px] pl-[8px] font-medium">Sign up</Link>
+                <button className="sm:text-[3rem] sm:px-[2rem] sm:py-[1rem] sm:leading-[6rem] sm:rounded-[1rem] text-[9rem] w-full leading-[11rem] py-[5rem] rounded-[3rem] primary-btn ">Sign In</button>
+                <div className=" sm:mt-[4rem] sm:gap-[2rem] flex justify-center items-center gap-[5rem] text-[7rem] mt-[13rem]">
+                    <p className=" sm:text-[3rem] text-[#828A91]">Don’t have an account?</p>
+                    <Link href="#" className="sm:text-[3rem] font-semibold">Sign up</Link>
                 </div>
             </div>
         </div>
