36c92744d2a2749f628dddb0010d7b6f7e24f1ba
Pav <morgunpavlo@gmail.com>
2024-04-11 21:03:51 -0500

    Updated FAQ on docs

--- app/(Header and footer)/docs/page.tsx
+++ app/(Header and footer)/docs/page.tsx
@@ -4,19 +4,15 @@ import { Header3 } from "@/app/components/Header3/page"
 import { Content } from "@/app/components/content/page"
 import { Header1 } from "@/app/components/header1/page"
 import { Header2 } from "@/app/components/header2/page"
-import { Question } from "@/app/components/question/page"
+
 import { Session1 } from "@/app/components/session1/page"
 import { Session2 } from "@/app/components/session2/page"
 import { Session3 } from "@/app/components/session3/page"
-import plus from "../../../public/Group 1000001523.svg"
-import minus from "../../../public/minus.svg"
+
 import React, { useContext } from "react"
-import { Space_Grotesk } from "next/font/google"
 import { Context } from "@/app/components/context/context"
 import { Header4 } from "@/app/components/Header4/page"
-
-
-const space_grotesk = Space_Grotesk({ subsets: ["latin"] });
+import { FAQ } from "@/app/components/FAQ/page"
 
 export default (props:any)=> {
 
@@ -63,29 +59,10 @@ export default (props:any)=> {
                         <Content text = "It's time to step into a new era of data extraction with JSON Scout where the insights you seek are just a query away.Make the shift." />
                         <Header3 text = "Easy steps" />
                         <Content text = "JSON Scout saves development time and maintenance by comparing the old way of doing things vs using it JSON Scout saves development time and maintenance by comparing the old way of doing things vs using it" />
-                        
-                        
-                    
-                    
-                    
-                    
-                    
                     </div>
                 </div>
             </div>
-            <p className={`sm:text-[6rem] sm:leading-[8rem] sm:mt-[0rem] text-[16rem] mt-[27rem] text-center leading-[20rem] font-semibold ${space_grotesk.className}`}>Frequently Asked Questions</p>
-                    <div className="sm:mt-[9rem] sm:items-center sm:flex-row sm:px-[10rem] sm:gap-[4rem] flex flex-col justify-start gap-[6rem] px-[20px] mt-[18rem] mb-[12rem]">
-                        <div className="sm:w-[48%] sm:gap-[4rem] sm:justify-start flex flex-col justify-start gap-[6rem]">
-                            <Question img1={plus} img2={minus} question="How do you determine the correct data to extract?" answer="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush. Merits behind on afraid or warmly." n = {1} selected = {order} change={setOrder}/>
-                            <Question img1={plus} img2={minus} question="How do you determine the correct data to extract?" answer="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush. Merits behind on afraid or warmly." n = {2} selected = {order} change={setOrder} />
-                            
-                        </div>
-                        <div className="sm:w-[48%] sm:gap-[4rem] sm:justify-start flex flex-col justify-start gap-[6rem]">
-                            <Question img1={plus} img2={minus} question="How do you determine the correct data to extract?" answer="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush. Merits behind on afraid or warmly." n = {3} selected = {order} change={setOrder} />
-                            <Question img1={plus} img2={minus} question="How do you determine the correct data to extract?" answer="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush. Merits behind on afraid or warmly." n = {4} selected = {order} change={setOrder} />
-                            <Question img1={plus} img2={minus} question="How do you determine the correct data to extract?" answer="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush. Merits behind on afraid or warmly." n = {5} selected = {order} change={setOrder} />
-                        </div>
-            </div> 
+            <FAQ/>
         </>
     )
 }
