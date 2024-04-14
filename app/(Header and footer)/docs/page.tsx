71a93dec822d8e192e7667a4f07842b3862f5044
Pav <morgunpavlo@gmail.com>
2024-04-13 22:22:05 -0500

    Updated api docs

--- app/(Header and footer)/docs/page.tsx
+++ app/(Header and footer)/docs/page.tsx
@@ -61,28 +61,26 @@ export default (props:any)=> {
                 <div className={`sm:w-[20%] w-full sm:relative fixed left-0 top-0 bottom-0 bg-[#0007] z-[100] sm:block ${show?"":"hidden"}`}>
                     <div ref={sidebarRef} className={`sm:w-full w-[max-content] h-full bg-white px-[10rem] pt-[20rem] sm:p-0 ${show?"animate-show":"animate-hide"}`}>
                         <Session1 text = "Getting Started" />
-                        <Session2 text = "Installation" />
-                        <Session2 text = "Installation" />
-                        <Session1 text = "Build your application" />
-                        <Session2 text = "Installation" />
-                        <Session2 text = "Installation" />
+                        <Session2 text = "API Key" />
+                        <Session2 text = "Basic Request" />
+                        <Session2 text = "Batch Limits" />
                     </div>
                 </div>
                 <div className="sm:w-[76%] w-full flex justify-between">
                     <div className="sm:w-[75%] w-full">
                         <Header1 text = "Introduction" />
-                        <Header4 text = "Welcome to JSONScout Documentation!" />
-                        <Header2 text = "What is JSONScout?" />
-                        <Content text = "It's time to step into a new era of data extraction with JSON Scout where the insights you seek are just a query away.Make the shift." />
-                        <Content text = "It's time to step into a new era of data extraction with JSON Scout where the insights you seek are just a query away.Make the shift." />
-                        <Content text = "It's time to step into a new era of data extraction with JSON Scout where the insights you seek are just a query away.Make the shift." />
-                        <div className="sm:mt-[10rem] sm:mb-[8rem]"><CustomCodeBlock code = {code1} centerTitle="New Method" leftTitle="EXAMPLE" rightTitle={["CURL", "node.js", "python"]}/></div>
-                        <Header2 text = "What is JSONScout?" />
-                        <Content text = "It's time to step into a new era of data extraction with JSON Scout where the insights you seek are just a query away.Make the shift." />
-                        <Content text = "It's time to step into a new era of data extraction with JSON Scout where the insights you seek are just a query away.Make the shift." />
-                        <Content text = "It's time to step into a new era of data extraction with JSON Scout where the insights you seek are just a query away.Make the shift." />
-                        <Header3 text = "Easy steps" />
-                        <Content text = "JSON Scout saves development time and maintenance by comparing the old way of doing things vs using it JSON Scout saves development time and maintenance by comparing the old way of doing things vs using it" />
+                        <Header4 text = "Welcome to JSON Scout Documentation!" />
+                        <Header2 text = "Get your API Key" />
+                        <Content text = "Visit your profile page to get your API key. This will allow you to integrate JSON Scout with your data pipeline." />
+                        <Header2 text = "Basic Request" />
+                        <Content text = "Below is an example of how to send a basic request for one item" />
+                        <Content text = "Simply provide our api key, the desired outputs, your content within the character limit and it will return a structured json back." />
+                        <Content text = "There is no rate limit on how many requests you send, just a token and character limit that is part of your plan." />
+                        <div className="sm:mt-[10rem] sm:mb-[8rem]">
+                            <CustomCodeBlock code = {code1} centerTitle="" leftTitle="EXAMPLE" rightTitle={["CURL", "node.js", "python"]}/>
+                        </div>
+                        <Header2 text = "Batch Limits" />
+                        <Content text = "On some plans, you are able to batch up to 500 items in your content. These types of requests will use our parallel processing architecture." />
                     </div>
                 </div>
             </div>
